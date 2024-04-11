51e8705c666d79e2cfc0f9df2a130df0c0f8ddb2
grig <prowlergrig@yahoo.ie>
2024-04-11 22:11:46 +0300

    Update WiFiConfig.h

--- WiFiConfig.h
+++ WiFiConfig.h
@@ -2,14 +2,14 @@
 #define _WIFI_CONFIG_H_
 
 /* Place your WiFi SSID here */
-#define WIFI_SSID  "TP-Link_0A36"
+#define WIFI_SSID  "Conference"
 /* Place your WiFi password here */
-#define WIFI_PASSWORD "39628671"
+#define WIFI_PASSWORD "politehnica"
 
 /* Put the MAX/MSP host port here. Use commas to separate IP values instead of dots */
-#define WIFI_MAX_HOST 192,168,0,104
+#define WIFI_MAX_HOST 192,168,126,165
 
 /* Put the UDP port where MAX/MSP is listening for OSC messages */
-#define WIFI_MAX_PORT 8888
+#define WIFI_MAX_PORT 8889
 
 #endif
